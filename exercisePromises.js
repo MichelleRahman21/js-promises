fc70198c9e37b9a2aaf6fa4e7d3c776191a3c7c3
michelleRahman21 <michgomez1807@hotmail.com>
2020-02-13 13:02:55 -0500

    Answer Zero To Mastery js-promise exercise

--- exercisePromises.js
+++ exercisePromises.js
@@ -1,16 +1,21 @@
 // Solve the questions below:
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
-
+const promise = new Promise ((resolve, reject)) =>{
+  setTimeout(() => {
+    resolve('success!')
+  }, 4000)
+}
 // #2) Run the above promise and make it console.log "success"
-
-
+promise.then(console.log('success'))
+promise.then(response => console.log('response'))
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
 // the above promise shorter with Promise.resolve() and console loggin "success"
 
 
 // #4) Catch this error and console log 'Ooops something went wrong'
 Promise.reject('failed')
+  .catch(console.log('Oops something went wrong'))
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
@@ -20,6 +25,15 @@ const urls = [
   'https://swapi.co/api/people/3',
   'https://swapi.co/api/people/4'
 ]
-
+promise.all(urls.map(url =>
+  fetch(url).then(people => people.json())
+))
+.then(array => {
+  console.log('1', array[0])
+  console.log('2', array[1])
+  console.log('3', array[2])
+  console.log('4', array[3])
+})
+.catch(err => console.log('ugg fix it!', err))
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
